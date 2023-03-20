cb22536aebf33cad53217755b4fbe23ca5b9b09b
Brenopms <brenosilva1@gmail.com>
2023-03-20 18:00:44 -0300

    feat: update faq text

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,4 +1,4 @@
-function FAQ() {
+export const FAQ = () => {
   return (
     <div className="max-w-screen-xl p-8 mx-auto pb-52">
       <h2 className="mb-12 text-3xl font-extrabold leading-9 text-gray-900 dark:text-gray-100 underline underline-offset-6 decoration-8 decoration-brand">
@@ -7,77 +7,80 @@ function FAQ() {
       <ul className="flex flex-col items-start gap-8">
         <li>
           <p className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-100">
-            What is profit margin?
+            What is a control system?
           </p>
           <div className="mt-2 text-base leading-6 text-gray-500 dark:text-gray-400">
-            Profit margin indicates the profitability of a product, service, or
-            business. It’s expressed as a percentage; the higher the number, the
-            more profitable the business.
+            A control system manages, commands, directs, or regulates the
+            behavior of other devices or systems using control loops. It can
+            range from a single home heating controller using a thermostat
+            controlling a domestic boiler to large industrial control systems
+            which are used for controlling processes or machines. The control
+            systems are designed via control engineering process. Directly from
+            wikipedia - :)
           </div>
         </li>
         <li>
           <p className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-100">
-            How do you calculate gross profit margin?
+            What does this application do?
           </p>
           <div className="mt-2 text-base leading-6 text-gray-500 dark:text-gray-400">
-            It’s simple to find gross profit margin automatically using the
-            calculator. To calculate manually, subtract the cost of goods sold
-            (COGS) from the net sales (gross revenues minus returns, allowances,
-            and discounts). Then divide this figure by net sales, to calculate
-            the gross profit margin in a percentage. <br />
-            <br />
+            This application takes a <i>transfer function</i> and displays the
+            associated zeros and poles as well as plotting a couple of the more
+            common charts for Stability Analysis, Time-Domain and
+            Frequency-Domain behavior
+          </div>
+        </li>
+        <li>
+          <p className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-100">
+            How does it work?
+          </p>
+          <div className="mt-2 text-base leading-6 text-gray-500 dark:text-gray-400">
+            You first have to input the transfer function numerator and
+            denominator. This can be done writing numbers split by a ";". The
+            first number will be the highest coefficient of the expression. A
+            "1;2;3" represents the expression: "s² + 2s + 3".
+          </div>
+        </li>
+        <li>
+          <p className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-100">
+            But why?
+          </p>
+          <div className="mt-2 text-base leading-6 text-gray-500 dark:text-gray-400">
+            This is actually just a demo for demonstrating the functionalities
+            of a{" "}
             <a
-              href="https://www.youtube.com/@codeofrelevancy"
-              target="_blank"
-              className="text-brand"
+              href="https://github.com/Brenopms/systems-controls-js"
+              className="text-brand hover:underline"
             >
-              Code of Relevancy
-            </a>
-            's free profit margin calculator does it for you, but you can also
-            use the following formula:
-            <br />
-            Step 1: X (Net sales) - Y (COGS) = Z<br />
-            Step 2: Z / X (Net sales) = % Gross profit margin
+              Typescript library
+            </a>{" "}
+            built for aiding in system control analysis. There are just visual
+            examples of the data generated using the package.
           </div>
         </li>
         <li>
           <p className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-100">
-            How do you calculate a 20% profit margin?
+            How can I contribute?
           </p>
           <div className="mt-2 text-base leading-6 text-gray-500 dark:text-gray-400">
-            Follow these easy steps to calculate a 20% profit margin:
-            <ol className="list-decimal list-inside">
-              <li>Use 20% in its decimal form, which is 0.2</li>
-              <li>Subtract 0.2 from 1 to get 0.8</li>
-              <li>Divide the original price of your good by 0.8</li>
-              <li>
-                The resulting number is how much you should charge for a 20%
-                profit margin
-              </li>
-            </ol>
-          </div>
-        </li>
-        <li>
-          <p className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-100">
-            How does the profit margin calculator work?
-          </p>
-          <div className="mt-2 text-base leading-6 text-gray-500 dark:text-gray-400">
+            You can check, make questions, open issues and pull requests in the{" "}
             <a
-              href="https://www.youtube.com/@codeofrelevancy"
-              target="_blank"
-              className="text-brand"
+              href="https://github.com/Brenopms/systems-controls-js"
+              className="text-brand hover:underline"
             >
-              Code of Relevancy
+              original repository
             </a>
-            ’s free profit margin calculator is fast and easy to use. It simply
-            takes the markup percentage you enter, adds that in decimal format
-            to the cost, and then gives you a sale price which reflects the
-            markup.
+            . You can also check the code for this example{" "}
+            <a
+              href="https://github.com/Brenopms/systems-controls-js-demo"
+              className="text-brand hover:underline"
+            >
+              here
+            </a>
+            .
           </div>
         </li>
       </ul>
     </div>
   );
-}
-
-export default FAQ;
+};
